d4a1bb8aa49e4ad4d70cfb3e7b1a8ce9fa19469c
dieyi <dieyidezui@gmail.com>
2018-11-22 23:00:44 +0800

    Feat: add priority for TryCatchHandler

--- lancet-base/src/main/java/com/dieyidezui/lancet/rt/annotations/TryCatchHandler.java
+++ lancet-base/src/main/java/com/dieyidezui/lancet/rt/annotations/TryCatchHandler.java
@@ -14,4 +14,9 @@ import java.lang.annotation.*;
 @Target(ElementType.METHOD)
 @AutoReplaced
 public @interface TryCatchHandler {
+
+    /**
+     * @return priority
+     */
+    int priority() default 0;
 }
