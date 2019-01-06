2bd18e517f42e8df23e22b5928825ee4735b776e
dieyi <dieyidezui@gmail.com>
2019-01-06 19:08:34 +0800

    Feat: add empty plugin

---
+++ samples/SafeCatcher/safecatcher/src/main/java/coffeepartner/capt/sample/safecatcher/TestPlugin.java
@@ -0,0 +1,18 @@
+package coffeepartner.capt.sample.safecatcher;
+
+import java.io.IOException;
+
+import coffeepartner.capt.plugin.api.Capt;
+import coffeepartner.capt.plugin.api.Plugin;
+import coffeepartner.capt.plugin.api.annotations.Def;
+
+/**
+ * Created by luoye on 2019/1/6.
+ */
+@Def
+public class TestPlugin extends Plugin<Capt> {
+    @Override
+    public void onCreate(Capt capt) throws IOException, InterruptedException {
+
+    }
+}
